837788380ef5812bc0948bd124a1959372021ba1
kyledavisdev <kyledavisdev@gmail.com>
2021-07-03 09:50:52 -0700

    init commit

---
+++ KyleKoozies/ClientApp/src/components/Grid/GridContainer.test.tsx
@@ -0,0 +1,24 @@
+﻿import * as React from 'react';
+import * as enzyme from 'enzyme';
+import casual from 'casual'
+import GridContainer, {IGridContainerProps} from './GridContainer';
+
+const fakeJSXElement = (): JSX.Element => {
+    return casual.random_element([
+        React.createElement("div", { key: casual.uuid }, casual.string),
+        React.createElement("span", { key: casual.uuid }, casual.string)
+    ]);
+};
+
+const fakeGrid = (): IGridContainerProps => ({
+    children: fakeJSXElement(), 
+    className: casual.sentence
+});
+
+describe("<GridContainer />", () => {
+    it('renders without crashing', () => {
+        const wrapper = enzyme.shallow(<GridContainer><p>hi</p></GridContainer>);
+        console.log(wrapper);
+        expect(wrapper).toBeTruthy();
+    });    
+})
---
+++ KyleKoozies/ClientApp/src/components/Grid/GridContainer.tsx
@@ -0,0 +1,33 @@
+﻿import * as React from "react";
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+const styles = {
+    grid: {
+        marginRight: "-15px",
+        marginLeft: "-15px",
+        width: "auto",
+    },
+};
+
+const useStyles = makeStyles(styles);
+
+export interface IGridContainerProps {
+    children: string | JSX.Element | Array<string | JSX.Element>;
+    className?: string | null;
+    [key: string]: any;
+}
+
+const GridContainer: React.FC<IGridContainerProps> = props => {
+    const { children, className, ...rest } = props;
+    const classes = useStyles();
+    return (
+        <Grid container {...rest} className={classes.grid + " " + className}>
+            {children}
+        </Grid>
+    );
+}
+
+export default GridContainer;
