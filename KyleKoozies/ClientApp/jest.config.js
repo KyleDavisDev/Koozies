33b4031143aee330321fcf6900a499810f8b68bb
kyledavisdev <kyledavisdev@gmail.com>
2021-07-03 09:51:50 -0700

    init commit

---
+++ KyleKoozies/ClientApp/jest.config.js
@@ -0,0 +1,17 @@
+﻿module.exports = {
+  setupFilesAfterEnv: [require.resolve("./jest.setup.ts")],
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  roots: ["<rootDir>/src/components"],
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+      tsConfig: require.resolve("./tsconfig.test.json")
+    }
+  }
+};
---
+++ KyleKoozies/ClientApp/jest.setup.ts
@@ -0,0 +1,4 @@
+﻿import enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+enzyme.configure({ adapter: new Adapter() });
