d3ba7e3bf16c7402c6a996075bf0d501d9655820
kyledavisdev <kyledavisdev@gmail.com>
2021-07-07 15:06:57 -0700

    init commit

---
+++ KyleKoozies/ClientApp/src/pages/Home/components/LatestOffers/LatestOffersStyles.ts
@@ -0,0 +1,49 @@
+﻿import {
+  section,
+  container,
+  cardTitle,
+  coloredShadow,
+  grayColor,
+  dangerColor,
+  mlAuto,
+} from "../../../../theme/theme";
+
+import tooltipsStyle from "../../../../components/ToolTip/ToolTipStyles";
+
+const LatestOfferStyles = {
+  cardTitle,
+  container,
+  ...tooltipsStyle,
+  section: {
+    ...section,
+    padding: "70px 0px",
+  },
+  coloredShadow,
+  cardDescription: {
+    color: grayColor[0],
+    textAlign: "center" as "center",
+  },
+  mlAuto,
+  priceContainer: {
+    display: "inline-flex",
+  },
+  price: {
+    fontSize: "18px",
+    color: grayColor[22],
+  },
+  priceOld: {
+    fontSize: "16px",
+    textDecoration: "line-through",
+  },
+  priceNew: {
+    color: dangerColor[0],
+  },
+  stats: {
+    color: grayColor[0],
+  },
+  textCenter: {
+    textAlign: "center" as "center",
+  },
+};
+
+export default LatestOfferStyles;
