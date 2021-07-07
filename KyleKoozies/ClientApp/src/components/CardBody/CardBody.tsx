6644d24d53b7425ac631b6deabd60a1708d1bb85
kyledavisdev <kyledavisdev@gmail.com>
2021-07-07 13:46:12 -0700

    init commit

---
+++ KyleKoozies/ClientApp/src/components/CardBody/CardBody.tsx
@@ -0,0 +1,58 @@
+﻿import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// @material-ui/icons
+
+// core components
+import CardBodyStyles from "./CardBodyStyles";
+
+const useStyles = makeStyles(CardBodyStyles);
+
+export interface ICardBody {
+  className?: string;
+  background?: boolean;
+  plain?: boolean;
+  formHorizontal?: boolean;
+  pricing?: boolean;
+  signup?: boolean;
+  color?: boolean;
+  children?: Element | JSX.Element | Array<Element | JSX.Element>;
+}
+
+const CardBody: React.FC<ICardBody> = props => {
+  const {
+    className = "",
+    children,
+    background,
+    plain,
+    formHorizontal,
+    pricing,
+    signup,
+    color,
+    ...rest
+  } = props;
+
+  const classes = useStyles();
+  const cardBodyClasses = classNames({
+    [classes.cardBody]: true,
+    [classes.cardBodyBackground]: background,
+    [classes.cardBodyPlain]: plain,
+    [classes.cardBodyFormHorizontal]: formHorizontal,
+    [classes.cardPricing]: pricing,
+    [classes.cardSignup]: signup,
+    [classes.cardBodyColor]: color,
+    [className]: className !== undefined
+  });
+
+  return (
+    <div className={cardBodyClasses} {...rest}>
+      {children}
+    </div>
+  );
+};
+
+export default CardBody;
