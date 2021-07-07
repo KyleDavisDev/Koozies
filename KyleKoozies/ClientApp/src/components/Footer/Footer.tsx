2d84081b592908312561f292d6a7d23fec64d978
kyledavisdev <kyledavisdev@gmail.com>
2021-07-07 14:48:19 -0700

    init commit

---
+++ KyleKoozies/ClientApp/src/components/Footer/Footer.tsx
@@ -0,0 +1,47 @@
+﻿import React from "react";
+import classNames from "classnames";
+import { makeStyles } from "@material-ui/core/styles";
+
+import FooterStyles from "./FooterStyles";
+
+const useStyles = makeStyles(FooterStyles);
+
+export interface IFooter {
+  theme?: "dark" | "white" | "transparent";
+  big?: boolean;
+  content: Element | JSX.Element | Array<Element | JSX.Element>;
+  children: Element | JSX.Element | Array<Element | JSX.Element>;
+  className?: string;
+}
+
+const Footer: React.FC<IFooter> = props => {
+  const { children, content, theme = "dark", big = true, className = "" } = props;
+
+  const classes = useStyles();
+  const themeType = (theme && theme !== "transparent");
+  const footerClasses = classNames({
+    [classes.footer]: true,
+    [classes[theme]]: themeType,
+    [classes.big]: big || children !== undefined,
+    [className]: className !== undefined
+  });
+
+  return (
+    <footer className={footerClasses}>
+      <div className={classes.container}>
+        {children !== undefined ? (
+          <div>
+            <div className={classes.content}>{children}</div>
+            <hr />
+          </div>
+        ) : (
+          " "
+        )}
+        {content}
+        <div className={classes.clearFix} />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ KyleKoozies/ClientApp/src/components/Footer/FooterStyles.ts
@@ -0,0 +1,167 @@
+﻿import {
+  container,
+  primaryColor,
+  whiteColor,
+  btnLink,
+  grayColor,
+  twitterColor,
+  dribbbleColor,
+  instagramColor,
+  hexToRgb
+} from "../../theme/theme";
+
+const FooterStyles = {
+  left: {
+    float: "left !important" as "left",
+    display: "block" as "block"
+  },
+  right: {
+    padding: "15px 0",
+    margin: "0",
+    float: "right" as "right"
+  },
+  rightLinks: {
+    float: "right !important" as "right",
+    "& ul": {
+      marginBottom: 0,
+      marginTop: 10,
+      padding: 0,
+      listStyle: "none",
+      height: 38,
+      "& li": {
+        display: "inline-block"
+      }
+    },
+    "& i": {
+      fontSize: "20px"
+    }
+  },
+  footer: {
+    padding: "0.9375rem 0",
+    textAlign: "center" as "center",
+    display: "flex",
+    zIndex: 2,
+    position: "relative" as "relative",
+    "& ul": {
+      marginBottom: "0",
+      padding: 0,
+      listStyle: "none"
+    }
+  },
+  big: {
+    padding: "1.875rem 0",
+    "& h5, & h4": {
+      fontWeight: 700,
+      fontFamily: "Roboto Slab,Times New Roman,serif",
+      marginBottom: "15px"
+    },
+    "& p": {
+      color: grayColor[0]
+    }
+  },
+  content: {
+    textAlign: "left" as "left"
+  },
+  a: {
+    color: primaryColor[0],
+    textDecoration: "none",
+    backgroundColor: "transparent"
+  },
+  dark: {
+    background: "radial-gradient(ellipse at center," + grayColor[4] + " 0," + grayColor[5] + " 100%)",
+    backgroundSize: "550% 450%",
+    color: whiteColor,
+    "& p": {
+      color: grayColor[0]
+    },
+    "& i": {
+      color: whiteColor
+    },
+    "& a": {
+      color: whiteColor,
+      opacity: ".86",
+      "&:visited": {
+        color: whiteColor
+      },
+      "&:focus, &:hover": {
+        opacity: 1
+      }
+    },
+    "& hr": {
+      borderColor: "rgba(" + hexToRgb(whiteColor) + ",0.2)"
+    },
+    "& $btnTwitter, & $btnDribbble, & $btnInstagram": {
+      color: whiteColor
+    }
+  },
+  white: {
+    backgroundColor: whiteColor,
+    color: grayColor[1],
+    textDecoration: "none",
+    "& a": {
+      "&:visited": {
+        color: grayColor[1]
+      },
+      "&:hover, &:focus": {
+        color: grayColor[20]
+      }
+    }
+  },
+  container,
+  list: {
+    marginBottom: "0",
+    padding: "0",
+    marginTop: "0"
+  },
+  inlineBlock: {
+    display: "inline-block",
+    padding: "0px",
+    width: "auto"
+  },
+  icon: {
+    width: "18px",
+    height: "18px",
+    position: "relative" as "relative",
+    top: "3px"
+  },
+  iconSocial: {
+    width: "41px",
+    height: "41px",
+    fontSize: "24px",
+    minWidth: "41px",
+    padding: 0,
+    overflow: "hidden",
+    position: "relative" as "relative"
+  },
+  btnTwitter: {
+    ...btnLink,
+    color: twitterColor
+  },
+  btnDribbble: {
+    ...btnLink,
+    color: dribbbleColor
+  },
+  btnInstagram: {
+    ...btnLink,
+    color: instagramColor
+  },
+  footerBrand: {
+    height: "50px",
+    padding: "15px 15px",
+    fontSize: "18px",
+    lineHeight: "50px",
+    marginLeft: "-15px",
+    color: grayColor[1],
+    textDecoration: "none",
+    fontWeight: 700,
+    fontFamily: "Roboto Slab,Times New Roman,serif"
+  },
+  pullCenter: {
+    display: "inline-block",
+    float: "none" as "none"
+  },
+  clearFix: {
+    clear: "both" as "both"
+  }
+};
+export default FooterStyles;
