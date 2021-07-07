f14ea37a861cf6541e61fb09dfe6b6670585ddb6
kyledavisdev <kyledavisdev@gmail.com>
2021-07-07 13:33:28 -0700

    init commit

---
+++ KyleKoozies/ClientApp/src/components/Card/Card.tsx
@@ -0,0 +1,69 @@
+﻿import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// @material-ui/icons
+
+// core components
+import CardStyles from "./CardStyles";
+
+const useStyles = makeStyles(CardStyles);
+
+export interface ICard {
+  className: string;
+  plain?: boolean;
+  profile?: boolean;
+  blog?: boolean;
+  raised?: boolean;
+  background?: boolean;
+  pricing?: boolean;
+  testimonial?: boolean;
+  color: "primary" | "info" | "success" | "warning" | "danger" | "rose";
+
+  product?: boolean;
+  children: Element | JSX.Element | Array<Element | JSX.Element>;
+}
+
+const Card: React.FC<ICard> = (props) => {
+  const {
+    className,
+    children,
+    plain,
+    profile,
+    blog,
+    raised,
+    background,
+    pricing,
+    color,
+    product,
+    testimonial,
+    ...rest
+  } = props;
+  
+  const classes = useStyles();
+  const cardClasses = classNames({
+    [classes.card]: true,
+    [classes.cardPlain]: plain,
+    [classes.cardProfile]: profile || testimonial,
+    [classes.cardBlog]: blog,
+    [classes.cardRaised]: raised,
+    [classes.cardBackground]: background,
+    [classes.cardPricingColor]:
+      (pricing && color !== undefined) || (pricing && background !== undefined),
+    [classes[color]]: color,
+    [classes.cardPricing]: pricing,
+    [classes.cardProduct]: product,
+    [className]: className !== undefined
+  });
+  
+  return (
+    <div className={cardClasses} {...rest}>
+      {children}
+    </div>
+  );
+}
+
+export default Card;
