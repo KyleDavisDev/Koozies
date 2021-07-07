8d988eff9a6a08c3369772ebea3d72e3c6e6bd46
kyledavisdev <kyledavisdev@gmail.com>
2021-07-07 10:34:13 -0700

    init commit

---
+++ KyleKoozies/ClientApp/src/components/Parallax/Parallax.tsx
@@ -0,0 +1,76 @@
+﻿import React from "react";
+import classNames from "classnames";
+import { makeStyles } from "@material-ui/core/styles";
+
+import ParallaxStyles from "./ParallaxStyles";
+const useStyles = makeStyles(ParallaxStyles);
+
+export interface IParallax {
+  className: string;
+  filter:
+    | "primary"
+    | "rose"
+    | "dark"
+    | "info"
+    | "success"
+    | "warning"
+    | "danger";
+  children: Node;
+  style: object;
+  image: string;
+  small: boolean;
+}
+
+const Parallax: React.FC<IParallax> = (props) => {
+  let windowScrollTop;
+  if (window.innerWidth >= 768) {
+    windowScrollTop = window.pageYOffset / 3;
+  } else {
+    windowScrollTop = 0;
+  }
+
+  const [transform, setTransform] = React.useState(
+    "translate3d(0," + windowScrollTop + "px,0)"
+  );
+
+  React.useEffect(() => {
+    if (window.innerWidth >= 768) {
+      window.addEventListener("scroll", resetTransform);
+    }
+    return function cleanup() {
+      if (window.innerWidth >= 768) {
+        window.removeEventListener("scroll", resetTransform);
+      }
+    };
+  });
+
+  const resetTransform = () => {
+    var windowScrollTop = window.pageYOffset / 3;
+    setTransform("translate3d(0," + windowScrollTop + "px,0)");
+  };
+
+  const { filter, className, children, style, image, small } = props;
+  
+  const classes = useStyles();
+  const parallaxClasses = classNames({
+    [classes.parallax]: true,
+    [classes[filter + "Color"]]: filter !== undefined,
+    [classes.small]: small,
+    [className]: className !== undefined
+  });
+  
+  return (
+    <div
+      className={parallaxClasses}
+      style={{
+        ...style,
+        backgroundImage: "url(" + image + ")",
+        transform: transform
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Parallax;
