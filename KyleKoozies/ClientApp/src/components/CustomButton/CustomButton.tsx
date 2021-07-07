c78866eea662b238b235559cb4bc4248b6f187f2
kyledavisdev <kyledavisdev@gmail.com>
2021-07-07 09:10:18 -0700

    Init commit

---
+++ KyleKoozies/ClientApp/src/components/CustomButton/CustomButton.tsx
@@ -0,0 +1,89 @@
+﻿import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+import CustomButtonStyles from "./CustomButtonStyles";
+
+const useStyles = makeStyles(CustomButtonStyles);
+
+export interface ICustomButton {
+  color:
+    | "primary"
+    | "secondary"
+    | "info"
+    | "success"
+    | "warning"
+    | "danger"
+    | "rose"
+    | "white"
+    | "twitter"
+    | "facebook"
+    | "google"
+    | "linkedin"
+    | "pinterest"
+    | "youtube"
+    | "tumblr"
+    | "github"
+    | "behance"
+    | "dribbble"
+    | "reddit"
+    | "instagram"
+    | "transparent";
+  size: "sm" | "lg";
+  simple: boolean;
+  round: boolean;
+  fullWidth: boolean;
+  disabled: boolean;
+  block: boolean;
+  link: boolean;
+  justIcon: boolean;
+  fileButton: boolean;
+  children: Node;
+  className: string;
+}
+
+const CustomButton = React.forwardRef<HTMLButtonElement, ICustomButton>((props, ref) => {
+  const {
+    color,
+    round,
+    children,
+    fullWidth,
+    disabled,
+    simple,
+    size,
+    block,
+    link,
+    justIcon,
+    fileButton,
+    className,
+    ...rest
+  } = props;
+  const classes = useStyles();
+  const btnClasses = classNames({
+    [classes.button]: true,
+    [classes[size]]: size,
+    [classes[color]]: color,
+    [classes.round]: round,
+    [classes.fullWidth]: fullWidth,
+    [classes.disabled]: disabled,
+    [classes.simple]: simple,
+    [classes.block]: block,
+    [classes.link]: link,
+    [classes.justIcon]: justIcon,
+    [classes.fileButton]: fileButton,
+    [className]: className
+  });
+  return (
+    <Button {...rest} ref={ref} className={btnClasses}>
+      {children}
+    </Button>
+  );
+});
+
+export default CustomButton;
