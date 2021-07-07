eb3601d33ed3f6aba536fa40b581cb11e09bdbd1
kyledavisdev <kyledavisdev@gmail.com>
2021-07-07 15:07:20 -0700

    wip

--- KyleKoozies/ClientApp/src/pages/Home/components/LatestOffers/LatestOffers.tsx
+++ KyleKoozies/ClientApp/src/pages/Home/components/LatestOffers/LatestOffers.tsx
@@ -1,4 +1,4 @@
-﻿  import React from "react";
+﻿import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Tooltip from "@material-ui/core/Tooltip";
 import classNames from "classnames";
