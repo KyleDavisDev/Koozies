69af6beeed53a64ecd42d55d18c2ccee4c20b2ee
kyledavisdev <kyledavisdev@gmail.com>
2021-07-14 09:13:18 -0700

    Remove unused comments

--- KyleKoozies/ClientApp/src/components/Card/components/CardFooter/CardFooter.tsx
+++ KyleKoozies/ClientApp/src/components/Card/components/CardFooter/CardFooter.tsx
@@ -1,7 +1,5 @@
-﻿/* eslint-disable prettier/prettier */
-import React from "react";
+﻿import React from "react";
 import classNames from "classnames";
-import PropTypes from "prop-types";
 import {makeStyles} from "@material-ui/core/styles";
 
 import CardFooterStyles from "./CardFooterStyles";
