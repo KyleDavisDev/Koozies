9a10f586866905062822cddfcf044888b3c5c08a
kyledavisdev <kyledavisdev@gmail.com>
2021-07-14 09:27:33 -0700

    Init commit

---
+++ KyleKoozies/ClientApp/src/pages/Home/components/SectionHero/SectionHero.tsx
@@ -0,0 +1,36 @@
+﻿import * as React from "react";
+import classNames from "classnames";
+import { makeStyles } from "@material-ui/core";
+
+import Parallax from "../../../../components/Parallax/Parallax";
+import GridItem from "../../../../components/GridItem/GridItem";
+import GridContainer from "../../../../components/GridContainer/GridContainer";
+
+import SectionHeroStyles from "./SectionHeroStyles";
+
+const useStyles = makeStyles(SectionHeroStyles);
+
+export interface ISectionHero {}
+
+const SectionHero: React.FC<ISectionHero> = props => {
+  const classes = useStyles();
+
+  return (
+    <Parallax image={require("assets/img/examples/clark-street-merc.jpg").default} filter="dark" small>
+      <div className={classes.container}>
+        <GridContainer>
+          <GridItem md={8} sm={8} className={classNames(classes.mlAuto, classes.mrAuto, classes.textCenter)}>
+            <div className={classes.brand}>
+              <h1 className={classes.title}>Fantastic Koozies!</h1>
+              <h4>
+                Kyle's <b>amazing</b> collection of koozies
+              </h4>
+            </div>
+          </GridItem>
+        </GridContainer>
+      </div>
+    </Parallax>
+  );
+};
+
+export default SectionHero;
---
+++ KyleKoozies/ClientApp/src/pages/Home/components/SectionHero/SectionHeroStyles.tsx
@@ -0,0 +1,24 @@
+﻿import { container, description, main, mainRaised, mlAuto, mrAuto, title, whiteColor } from "../../../../theme/theme";
+
+const SectionHeroStyles = {
+  title,
+  main,
+  mainRaised,
+  mrAuto,
+  mlAuto,
+  description,
+  textCenter: {
+    textAlign: "center !important" as "center"
+  },
+  container: {
+    ...container,
+    zIndex: 2
+  },
+  brand: {
+    "& h1, & h4": {
+      color: whiteColor
+    }
+  }
+};
+
+export default SectionHeroStyles;
