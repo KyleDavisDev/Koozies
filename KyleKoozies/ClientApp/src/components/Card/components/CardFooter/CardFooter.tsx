e5c3f0fbd9584f8f08c80a40295385c310b39c03
kyledavisdev <kyledavisdev@gmail.com>
2021-07-14 09:09:46 -0700

    Init commit

---
+++ KyleKoozies/ClientApp/src/components/Card/components/CardFooter/CardFooter.tsx
@@ -0,0 +1,47 @@
+﻿/* eslint-disable prettier/prettier */
+import React from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import {makeStyles} from "@material-ui/core/styles";
+
+import CardFooterStyles from "./CardFooterStyles";
+
+const useStyles = makeStyles(CardFooterStyles);
+
+export interface ICardFooter {
+  className?: string,
+  plain?: boolean,
+  profile?: boolean,
+  pricing?: boolean,
+  testimonial?: boolean,
+  children: Element | JSX.Element | Array<Element | JSX.Element>,
+}
+
+const CardFooter: React.FC<ICardFooter> = (props) => {
+  const {
+    className = "",
+    children,
+    plain = true,
+    profile = false,
+    pricing = false,
+    testimonial = false,
+    ...rest
+  } = props;
+  
+  const classes = useStyles();
+  const cardFooterClasses = classNames({
+    [classes.cardFooter]: true,
+    [classes.cardFooterPlain]: plain,
+    [classes.cardFooterProfile]: profile || testimonial,
+    [classes.cardFooterPricing]: pricing,
+    [classes.cardFooterTestimonial]: testimonial,
+    [className]: className !== undefined,
+  });
+  return (
+    <div className={cardFooterClasses} {...rest}>
+      {children}
+    </div>
+  );
+}
+
+export default CardFooter;
