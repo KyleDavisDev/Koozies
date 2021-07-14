e56c5077d5852be2d5505b5bec8c44b620ac969a
kyledavisdev <kyledavisdev@gmail.com>
2021-07-14 09:28:11 -0700

    Abstract out section into own component, import component

--- KyleKoozies/ClientApp/src/pages/Home/Home.tsx
+++ KyleKoozies/ClientApp/src/pages/Home/Home.tsx
@@ -8,6 +8,7 @@ import GridItem from "../../components/GridItem/GridItem";
 import Parallax from "../../components/Parallax/Parallax";
 import Footer from "../../components/Footer/Footer";
 
+import SectionHero from "./components/SectionHero/SectionHero"
 import SectionLatestKoozies from "./components/SectionLatestKoozies/SectionLatestKoozies";
 import SectionProducts from "views/EcommercePage/Sections/SectionProducts.js";
 import SectionBlog from "views/EcommercePage/Sections/SectionBlog.js";
@@ -36,21 +37,9 @@ const Home: React.FC<IHome> = props => {
           color: "info"
         }}
       />
-      <Parallax image={require("assets/img/examples/clark-street-merc.jpg").default} filter="dark" small>
-        <div className={classes.container}>
-          <GridContainer>
-            <GridItem md={8} sm={8} className={classNames(classes.mlAuto, classes.mrAuto, classes.textCenter)}>
-              <div className={classes.brand}>
-                <h1 className={classes.title}>Fantastic Koozies!</h1>
-                <h4>
-                  Kyle's <b>amazing</b> collection of koozies
-                </h4>
-              </div>
-            </GridItem>
-          </GridContainer>
-        </div>
-      </Parallax>
-
+      
+      <SectionHero />      
+      
       <div className={classNames(classes.main, classes.mainRaised)}>
         <SectionLatestKoozies />
         <SectionProducts />
