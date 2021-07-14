b8d39a0de4cc0f240468bd65331d66ea4b73c8f4
kyledavisdev <kyledavisdev@gmail.com>
2021-07-14 08:07:03 -0700

    Move responsibilities away from component, resolve errors

--- KyleKoozies/ClientApp/src/pages/Home/Home.tsx
+++ KyleKoozies/ClientApp/src/pages/Home/Home.tsx
@@ -1,75 +1,49 @@
 ﻿import React from "react";
 import classNames from "classnames";
 import { makeStyles } from "@material-ui/core/styles";
-import { List, ListItem } from "@material-ui/core";
 
-import Header from "../components/Header/Header";
-import GridContainer from "../components/GridContainer/GridContainer";
-import GridItem from "../components/GridItem/GridItem";
-import Parallax from "../components/Parallax/Parallax";
-import Footer from "../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import GridContainer from "../../components/GridContainer/GridContainer";
+import GridItem from "../../components/GridItem/GridItem";
+import Parallax from "../../components/Parallax/Parallax";
+import Footer from "../../components/Footer/Footer";
 
 import SectionLatestOffers from "views/EcommercePage/Sections/SectionLatestOffers.js";
 import SectionProducts from "views/EcommercePage/Sections/SectionProducts.js";
 import SectionBlog from "views/EcommercePage/Sections/SectionBlog.js";
 
+import HomeStyles from "./HomeStyles";
 
-import ecommerceHeader from "assets/img/examples/ecommerce-header.jpg";
-import face1 from "assets/img/faces/card-profile6-square.jpg";
-import face2 from "assets/img/faces/christian.jpg";
-import face3 from "assets/img/faces/card-profile4-square.jpg";
-import face4 from "assets/img/faces/card-profile1-square.jpg";
-import face5 from "assets/img/faces/marc.jpg";
-import face6 from "assets/img/faces/kendall.jpg";
-import face7 from "assets/img/faces/card-profile5-square.jpg";
-import face8 from "assets/img/faces/card-profile2-square.jpg";
+const useStyles = makeStyles(HomeStyles);
 
-import styles from "assets/jss/material-kit-pro-react/views/ecommerceStyle.js";
+export interface IHome {}
 
-const useStyles = makeStyles(styles);
-
-export interface IHome {};
-
-const Home: React.FC<IHome> = (props) => {
-  
+const Home: React.FC<IHome> = props => {
   React.useEffect(() => {
     window.scrollTo(0, 0);
     document.body.scrollTop = 0;
   });
-  
+
   const classes = useStyles();
   return (
     <div>
       <Header
-        brand="Material Kit PRO React"
+        brand="Koozies"
         fixed
         color="transparent"
         changeColorOnScroll={{
           height: 300,
-          color: "info",
+          color: "info"
         }}
       />
-      <Parallax
-        image={require("assets/img/examples/clark-street-merc.jpg").default}
-        filter="dark"
-        small
-      >
+      <Parallax image={require("assets/img/examples/clark-street-merc.jpg").default} filter="dark" small>
         <div className={classes.container}>
           <GridContainer>
-            <GridItem
-              md={8}
-              sm={8}
-              className={classNames(
-                classes.mlAuto,
-                classes.mrAuto,
-                classes.textCenter
-              )}
-            >
+            <GridItem md={8} sm={8} className={classNames(classes.mlAuto, classes.mrAuto, classes.textCenter)}>
               <div className={classes.brand}>
-                <h1 className={classes.title}>Ecommerce Page!</h1>
+                <h1 className={classes.title}>Fantastic Koozies!</h1>
                 <h4>
-                  Free global delivery for all products. Use coupon{" "}
-                  <b>25summer</b> for an extra 25% Off
+                  Kyle's <b>amazing</b> collection of koozies
                 </h4>
               </div>
             </GridItem>
@@ -82,188 +56,12 @@ const Home: React.FC<IHome> = (props) => {
         <SectionProducts />
       </div>
       <SectionBlog />
+
+      <Footer theme="dark" />
+        
       
-      <Footer
-        theme="dark"
-        content={
-          <div>
-            <div className={classes.left}>
-              <List className={classes.list}>
-                <ListItem className={classes.inlineBlock}>
-                  <a
-                    href="http://blog.creative-tim.com/?ref=mkpr-e-ecommerce"
-                    target="_blank"
-                    className={classes.block}
-                  >
-                    Blog
-                  </a>
-                </ListItem>
-                <ListItem className={classes.inlineBlock}>
-                  <a
-                    href="https://www.creative-tim.com/presentation?ref=mkpr-e-ecommerce"
-                    target="_blank"
-                    className={classes.block}
-                  >
-                    Presentation
-                  </a>
-                </ListItem>
-                <ListItem className={classes.inlineBlock}>
-                  <a
-                    href="#pablito"
-                    onClick={(e) => e.preventDefault()}
-                    className={classes.block}
-                  >
-                    Discover
-                  </a>
-                </ListItem>
-                <ListItem className={classes.inlineBlock}>
-                  <a
-                    href="#pablito"
-                    onClick={(e) => e.preventDefault()}
-                    className={classes.block}
-                  >
-                    Payment
-                  </a>
-                </ListItem>
-                <ListItem className={classes.inlineBlock}>
-                  <a
-                    href="https://www.creative-tim.com/contact-us?ref=mkpr-e-ecommerce"
-                    target="_blank"
-                    className={classes.block}
-                  >
-                    Contact us
-                  </a>
-                </ListItem>
-              </List>
-            </div>
-            <div className={classes.right}>
-              Copyright &copy; {1900 + new Date().getFullYear()}{" "}
-              <a
-                href="https://kyledavisdev.com"
-                target="_blank"
-                className={classes.aClasses}
-              >
-                Kyle Davis
-              </a>{" "}
-              All Rights Reserved.
-            </div>
-          </div>
-        }
-      >
-        <GridContainer>
-          <GridItem xs={12} sm={4} md={4}>
-            <h5>About Us</h5>
-            <p>
-              Creative Tim is a startup that creates design tools that make the
-              web development process faster and easier.{" "}
-            </p>
-            <p>
-              We love the web and care deeply for how users interact with a
-              digital product. We power businesses and individuals to create
-              better looking web projects around the world.{" "}
-            </p>
-          </GridItem>
-          <GridItem xs={12} sm={4} md={4}>
-            <h5>Social Feed</h5>
-            <div className={classes.socialFeed}>
-              <div>
-                <i className="fab fa-twitter" />
-                <p>How to handle ethical disagreements with your clients.</p>
-              </div>
-              <div>
-                <i className="fab fa-twitter" />
-                <p>The tangible benefits of designing at 1x pixel density.</p>
-              </div>
-              <div>
-                <i className="fab fa-facebook-square" />
-                <p>
-                  A collection of 25 stunning sites that you can use for
-                  inspiration.
-                </p>
-              </div>
-            </div>
-          </GridItem>
-          <GridItem xs={12} sm={4} md={4}>
-            <h5>Instagram Feed</h5>
-            <div className={classes.galleryFeed}>
-              <img
-                src={face1}
-                className={classNames(
-                  classes.img,
-                  classes.imgRaised,
-                  classes.imgRounded
-                )}
-                alt="..."
-              />
-              <img
-                src={face2}
-                className={classNames(
-                  classes.img,
-                  classes.imgRaised,
-                  classes.imgRounded
-                )}
-                alt="..."
-              />
-              <img
-                src={face3}
-                className={classNames(
-                  classes.img,
-                  classes.imgRaised,
-                  classes.imgRounded
-                )}
-                alt="..."
-              />
-              <img
-                src={face4}
-                className={classNames(
-                  classes.img,
-                  classes.imgRaised,
-                  classes.imgRounded
-                )}
-                alt="..."
-              />
-              <img
-                src={face5}
-                className={classNames(
-                  classes.img,
-                  classes.imgRaised,
-                  classes.imgRounded
-                )}
-                alt="..."
-              />
-              <img
-                src={face6}
-                className={classNames(
-                  classes.img,
-                  classes.imgRaised,
-                  classes.imgRounded
-                )}
-                alt="..."
-              />
-              <img
-                src={face7}
-                className={classNames(
-                  classes.img,
-                  classes.imgRaised,
-                  classes.imgRounded
-                )}
-                alt="..."
-              />
-              <img
-                src={face8}
-                className={classNames(
-                  classes.img,
-                  classes.imgRaised,
-                  classes.imgRounded
-                )}
-                alt="..."
-              />
-            </div>
-          </GridItem>
-        </GridContainer>
-      </Footer>
     </div>
   );
-}
+};
 
 export default Home;
